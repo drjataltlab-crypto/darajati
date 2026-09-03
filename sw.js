3d8cb8f2596297a636bee80491b521a160844d2e
drjataltlab-crypto <drjataltlab@gmail.com>
2026-09-03 13:59:23 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,37 @@
-var C = 'darajati-v1';
+var C = 'darajati-v2';
 var CORE = ['./', 'index.html', 'admin.html', 'icon.png', 'manifest.webmanifest', 'manifest-admin.webmanifest'];
+
 self.addEventListener('install', function (e) {
   e.waitUntil(caches.open(C).then(function (c) { return c.addAll(CORE); }));
   self.skipWaiting();
 });
-self.addEventListener('activate', function (e) { e.waitUntil(clients.claim()); });
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.filter(function (k) { return k !== C; }).map(function (k) { return caches.delete(k); }));
+  }).then(function () { return clients.claim(); }));
+});
+
 self.addEventListener('fetch', function (e) {
   if (e.request.method !== 'GET') return;
-  e.respondWith(caches.match(e.request).then(function (r) {
-    return r || fetch(e.request).then(function (res) {
-      if (res.ok && e.request.url.indexOf(self.location.origin) === 0) {
-        var cp = res.clone(); caches.open(C).then(function (c) { c.put(e.request, cp); });
-      }
+  var isPage = e.request.mode === 'navigate' || /\.html$/.test(new URL(e.request.url).pathname);
+  if (isPage) {
+    // الصفحات: يجلب أحدث نسخة من الإنترنت، وعند انقطاعه يفتح النسخة المحفوظة
+    e.respondWith(fetch(e.request).then(function (res) {
+      var cp = res.clone();
+      caches.open(C).then(function (c) { c.put(e.request, cp); });
       return res;
-    }).catch(function () { return caches.match('index.html'); });
-  }));
+    }).catch(function () { return caches.match(e.request); }));
+  } else {
+    // الصور والملفات الأخرى: من الذاكرة للأسرع
+    e.respondWith(caches.match(e.request).then(function (r) {
+      return r || fetch(e.request).then(function (res) {
+        if (res.ok && e.request.url.indexOf(self.location.origin) === 0) {
+          var cp = res.clone();
+          caches.open(C).then(function (c) { c.put(e.request, cp); });
+        }
+        return res;
+      });
+    }));
+  }
 });
