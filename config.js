93bbdc42bc702141504d0c90159340c397a69d3e
drjataltlab-crypto <drjataltlab@gmail.com>
2026-09-05 12:05:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
    هذا الملف الوحيد الذي يحتوي الرابط،
    ولا تحتاج لمسه عند أي تحديث مستقبلًا.
 */
-var SERVER_URL = "ضع_رابط_الخادم_هنا";
+var SERVER_URL = "https://script.google.com/macros/s/AKfycbzBp1YJOjJK6uCvB87R3qi0uaUc1bhpml3uOKqt2_8rvYcxkWVe9516Nr5BfWmcHqod/exec";
