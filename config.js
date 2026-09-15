54ce29876be914af353eb854611c353e54d4feb1
drjataltlab-crypto <drjataltlab@gmail.com>
2026-09-15 09:25:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
    هذا الملف الوحيد الذي يحتوي الرابط،
    ولا تحتاج لمسه عند أي تحديث مستقبلًا.
 */
-var SERVER_URL = "https://script.google.com/macros/s/AKfycbwTJMiVU-X9LgUwVOo9vyK5I2DI_EVsEFfJblxTDnSGmJiq1QIxRVuqzaBsasulBNgY/exec";
+var SERVER_URL = "https://script.google.com/macros/s/AKfycbyPW24RDVOTCW9iQ3rVot_pSz40qOY8Mwn8eM5DyLEvW2sAxdqZrBX_-P5zV8G5AQgm/exec";
